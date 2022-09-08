c17a3c2b338d112347d8edcbcfabb23a6025b7ab
Brian Leung <leungbk@posteo.net>
2022-09-08 10:14:09 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/subsystems/debian-control/translators/debian-control/parse-control-file.nix

--- src/subsystems/debian-control/translators/debian-control/parse-control-file.nix
+++ src/subsystems/debian-control/translators/debian-control/parse-control-file.nix
@@ -6,7 +6,19 @@ let
   controlFile = ./CONTROL.test;
   l = lib // builtins;
   lines = l.splitString "\n" controlFile;
-  foldl' () lines
+  desiredHeaders = ["Source" "Build-Depends" "Depends" "Recommends" "Suggests"];
+  # takeWhile = pred: list:
+    # foldl' (accum: line:
+    #   if pred line
+    #   then [head list] ++ accum
+    # else accum)
+
+    # if list == []
+    # then []
+    # else if pred (head list)
+    # then [head list] ++ takeWhile pred (tail list)
+
+  # foldl' () lines
   in
   {
 
